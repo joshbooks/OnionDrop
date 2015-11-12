0c4b0777d0aaabbb21b96fcc969134a9c70bcd9e
Virgil <i@virgil.gr>
2015-11-12 19:45:59 +0800

    address -> domain

--- forms.py
+++ forms.py
@@ -4,8 +4,8 @@ from wtforms.validators import Required
 
 class DropForm(Form):
 
-    field1 = TextField('.onion Address', validators=[Required()])
-    field2 = TextAreaField('Message', description='Copy paste encrypted text or enter your message plaintext and we\'ll encrypt it',
+    field1 = TextField('.onion domain', validators=[Required()])
+    field2 = TextAreaField('Message', description="Copy paste encrypted text or enter your message plaintext and we'll encrypt it",
                        validators=[Required()])
 
     submit_button = SubmitField('Drop Mail')
@@ -14,7 +14,7 @@ class GoDrop(Form):
     submit_button = SubmitField('Drop')
 
 class GetPack(Form):
-    field1 = TextField('.onion Address', validators=[Required()])	
+    field1 = TextField('.onion domain', validators=[Required()])	
     submit_button = SubmitField('Pickup')
 
 class GetAbout(Form):	
