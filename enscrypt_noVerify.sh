7595ecec5e8b9061963d4c56a59f5d2286885517
Josh <joshbooks@gmail.com>
2015-11-12 13:02:25 -0800

    Script to locally encrypt messages
    Just in case not everyone on earth trusts me implicitly

---
+++ enscrypt_noVerify.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#script to locally encrypt a message for onion drop
+#if you really really want to you can recompute the
+#hash of the public key and make sure it back comes out 
+#to the correct hostname, check the following webpage for
+#details:
+#https://trac.torproject.org/projects/tor/wiki/doc/HiddenServiceNames
+#only checks that curl and openssl are installed, assumes
+#tor socksPort is running on port 9050
+if [[ $# != 1 ]]
+then
+  echo "Usage: $0 .onion-domain"
+  exit
+fi
+host=$1
+if [[ ! -e `which openssl` ]]
+then
+  echo "openssl must be installed for this script to work"
+fi
+if [[ ! -e `which curl` ]]
+then
+  echo "curl must be installed for this script to work"
+fi
+
+curl -L --socks5-hostname localhost:9050 "oniondropodx6dyg.onion/key/$host" > ./"$host.tmp.pem"
+openssl rsa -pubin -RSAPublicKey_in -in ./"$host.tmp.pem" > ./"$host.pem"
+rm ./"$host.tmp.pem"
+echo "to encrypt a message with this key use the following command:"
+echo "openssl rsautl -encrypt -pubin -inkey $host.pem -in yourMessageFileHere"
+
+
